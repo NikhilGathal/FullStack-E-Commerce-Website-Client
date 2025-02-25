28c3d996bd8e38a84c053cbf4781388c5385c28d
Nikhil <nikhilgathal@gmail.com>
2025-02-25 11:50:34 +0530

    Final Commit

--- components/AddNewProduct.jsx
+++ components/AddNewProduct.jsx
@@ -123,7 +123,7 @@ const AddNewProduct = () => {
               value={newCategory}
               onChange={(e) => setNewCategory(e.target.value)}
             >
-              <option className='newpdt-inp' value="">Select Category</option>
+              <option className='newpdt-inp' hidden value="">Select Category</option>
               <option value="jewelery">Jewelery</option>
               <option value="men's clothing">Men's Clothing</option>
               <option value="electronics">Electronics</option>
